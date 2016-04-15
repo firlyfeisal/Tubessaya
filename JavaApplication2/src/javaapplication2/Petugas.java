0bb9d5fafb3be376bd67fef79ba84764d173cef8
krishajar <krishajar@kris>
2016-04-15 13:28:13 +0700

    Merge origin/master
    Conflicts:
    	JavaApplication1/src/javaapplication1/JavaApplication1.java

--- JavaApplication2/src/javaapplication2/Petugas.java
+++ JavaApplication2/src/javaapplication2/Petugas.java
@@ -1,24 +1,31 @@
 package javaapplication2;
 
+import java.util.*;
+
 public class Petugas{
-	private String nama;
-	private int id;
+    private String nama;
+    private int id;
+    private List<Gudang> gud = new ArrayList<>();
 	
-	public Petugas(int id, String nama){
+    public Petugas(int id, String nama){
 	this.id=id;
 	this.nama=nama;
-	}	        
+    }	        
         
-	public String getNama(){
-		return nama;
-	}	
-	public void setNama(String nama){
-		this.nama = nama;
-	}
-	public int getId(){
-		return id;
-	}
-	public void setId(int id){
-		this.id = id;
-	}
-}	
+    public String getNama(){
+	return nama;
+    }	
+    public void setNama(String nama){
+	this.nama = nama;
+    }
+    public int getId(){
+    	return id;
+    }
+    public void setId(int id){
+    	this.id = id;
+    }
+    public Gudang getGudang(int i){
+        Object[] array = gud.toArray();
+        return (Gudang) array[i];
+    }
+}
